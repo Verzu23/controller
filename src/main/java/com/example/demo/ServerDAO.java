3c3afa644727045c15595c04a636f5b79d237df2
Verzu23 <Giuseppe.vergari@outlook.it>
2019-06-01 20:53:03 +0200

    Second

--- src/main/java/com/example/demo/ServerDAO.java
+++ src/main/java/com/example/demo/ServerDAO.java
@@ -19,7 +19,7 @@ public class ServerDAO {
 
     public List<Light> getAll() {
 
-        List light = new ArrayList<Light>();
+        List<Light> light = new ArrayList<Light>();
         List<Map<String, Object>> rows = jdbcTemplate.queryForList(SQL);
 
 
